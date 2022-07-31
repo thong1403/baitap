9e7103609959209dd8f124516730c618dc9e3394
thong1403 <110025148+thong1403@users.noreply.github.com>
2022-07-31 22:13:56 +0700

    bài tập

---
+++ sesion 3/all.js
@@ -0,0 +1,147 @@
+//1: let a = prompt("người nhập một số n");
+// if (0 <= a && a <= 5){
+//      alert("nằm ở nửa số nhỏ");
+// }
+// else alert("nằm ở nửa số lớn");
+
+//2:
+
+// let a = prompt("cho người nhập một số bất kì n");
+// let b = prompt("nhập số bất kì x");
+
+// let c = a/2;
+// if(b < c){
+//     alert("nằm trong khoảng nhỏ từ o đến " + c);
+// }
+ 
+// else alert("nằm trong khoảng lớn ");
+
+// ex3
+// let a = Number(prompt("nhập một số tự nhiên"));
+// let b = Number(a ) % 2;
+
+// if(b===0){
+//     alert(`${b} là một số chẵn`);
+// }
+// else {
+//     alert(`${b}là một số lẻ`);
+// }
+
+// ex4
+// let a = prompt("nhập chiều cao M ");
+// let b = prompt("nhập cân nặng kg");
+// let c = Number(a /100);
+// let d = Number(b /( a * a));
+
+// if(d < 16){
+//     alert("mày là bộ sương ak");
+// }
+// else if (d > 16 && d < 18.5){
+//     alert("mày laf idol hàn quốc ak");
+// }
+// else if(d > 18.5 && d < 25){
+//     alert("mày đẹp trai đấy");
+// }
+// else if(d >25 && a< 30){
+//     alert("m béo rồi đấy");
+// }
+// else if( d > 30){
+//     alert("thôi m không cần dảm nữa đâu");
+// }
+
+//ex5
+
+// let a = Number(prompt("nhập số điểm môn hóa"));
+// let b = Number(prompt("nhập số điểm môn toán"));
+// let c = Number(prompt("nhập số điểm môn lý"));
+// let d = Number(prompt("nhập số điểm môn anh"));
+// let e = Number(prompt("nhập số điểm môn văn "));
+
+// let f = (( a + b +c +d +e)/50)*100;
+// if (f >= 90){
+//     alert(" Grade A");
+// }
+// else if(f>= 80 ){
+//     alert(" Grade B");
+// }
+// else if(f>= 70){
+//     alert(" Grade c");
+// }
+// else if(f >=60){
+//     alert("Grade D");
+// }
+// else if(f >= 40){
+//     alert("Grade e");
+// }
+// else {
+//     alert("Grade F");
+// }
+//ex6
+
+// let a = Number(prompt("nhập số năm"));
+// let b = (a % 4);
+
+
+// if(a > 0 && Number.isInteger(a) && b===0){
+//     alert("là năm nhuận");
+// }
+// else if(a>0 && Number.isInteger(a) && b != 0){
+//     alert("năm không nhuận");
+// }
+// else alert("không hợp lệ");
+
+//ex7
+//  let a = prompt("nhập vào chiều cao của bạn");
+// if(a < 150){
+//     alert("mày là người lùn ak");
+// }
+// else if(a > 150 && a < 169){
+//     alert("nhìn tàm tạm");
+// }
+// else {
+//     alert("trông m đc đấy");
+// }
+
+//ex8
+
+// let a = prompt("nhập vào 1 số tự nhiên");
+// let b = prompt(" nhập vào 1 số tự nhiên");
+
+// if(a>b){
+//     alert("số lớn nhất là " + a);
+// }
+// else {
+//     alert("số lớn nhất là " + b);
+// }
+
+//ex 9
+
+// let a = Number(prompt("cho người dùng nhập vào"));
+// let b = Number(prompt("cho người dùng nhập vào một số"));
+// let c = prompt("phép tính cần tính");
+// switch (c) {
+//     case "+":alert (a + b );
+    
+        
+//         break;
+//      case "-":alert (a - b);
+
+//      break;
+
+
+//      case" * ":alert (a * b);
+
+//      break;
+
+     
+
+    
+
+//     case "/":alert(a / b);
+//         break;
+//
+
+//10ex:
+
+
+ 
